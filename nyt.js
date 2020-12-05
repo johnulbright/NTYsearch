4708017952272e0c94edf2bb530aac7994797dfd
John Ulbright <john.ulbright@gmail.com>
2020-12-05 13:45:37 -0500

    updated lots of stuff

--- nyt.js
+++ nyt.js
@@ -1,7 +1,7 @@
 const baseURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json"; //1
 const key = "osevwPA4htbKB5VjXTIlAzOHAxK3vwgm"; //2
 let url; //3
-
+let hits;
 //SEARCH FORM
 const searchTerm = document.querySelector(".search");
 const startDate = document.querySelector(".start-date");
@@ -16,23 +16,27 @@ const nav = document.querySelector("nav");
 
 //RESULTS SECTION
 const section = document.querySelector("section");
-
-//nav.style.display = "none";
+const resultsInfo = document.querySelector(".resultsInfo");
 nextBtn.style.display = "none";
 previousBtn.style.display = "none";
 let pageNumber = 0;
-console.log("Page number:",pageNumber)
 let displayNav = false;
 
 //1                     //2
-searchForm.addEventListener("submit", fetchResults);
+searchForm.addEventListener("submit", newSearch);
 nextBtn.addEventListener("click", nextPage); //3
 previousBtn.addEventListener("click", previousPage); //3
 
+function newSearch(e) {
+  e.preventDefault();
+  pageNumber = 0;
+  nextBtn.style.display = "none";
+  previousBtn.style.display = "none";
+  fetchResults();
+}
 
 //1
 function fetchResults(e) {
-  e.preventDefault(); //2
   // Assemble the full URL
   url =
     baseURL +
@@ -42,17 +46,8 @@ function fetchResults(e) {
     pageNumber +
     "&q=" +
     searchTerm.value;
-    console.log("URL:", url);
-    fetch(url)
-    .then(function (result) {
-      console.log(result);
-      return result.json(); //2
-    })
-    .then(function (json) {
-      console.log(json); //3
-    });
+
   if (startDate.value !== "") {
-    console.log(startDate.value);
     url += "&begin_date=" + startDate.value;
   }
   if (endDate.value !== "") {
@@ -63,25 +58,37 @@ function fetchResults(e) {
       return result.json();
     })
     .then(function (json) {
+      hits = json.response.meta.hits;
       displayResults(json); //1 & //3
     });
 }
+
 function displayResults(json) {
-    while (section.firstChild) {
-        section.removeChild(section.firstChild); //1
-    }
+  while (section.firstChild) {
+    section.removeChild(section.firstChild); //1
+  }
   let articles = json.response.docs;
-  if (articles.length >= 10) {
+
+  if (hits === 0) {
+    resultsInfo.innerText = "No results found.";
+  } else if (hits < 10 * pageNumber + 10) {
+    resultsInfo.innerText = `Results ${pageNumber * 10 + 1}-${hits} of ${hits}`;
+  } else {
+    resultsInfo.innerText = `Results ${pageNumber * 10 + 1}-${
+      pageNumber * 10 + 10
+    } of ${hits}`;
+  }
+  if (hits > 10 * (pageNumber + 1)) {
     nextBtn.style.display = "block"; //shows the nav display if 10 items are in the array
   } else {
     nextBtn.style.display = "none"; //hides the nav display if less than 10 items are in the array
   }
-  if(pageNumber>0){
-      previousBtn.style.display="block";
-  } else{ 
-    previousBtn.style.display="none";
+  if (pageNumber > 0) {
+    previousBtn.style.display = "block";
+  } else {
+    previousBtn.style.display = "none";
   }
-  
+
   if (articles.length === 0) {
     console.log("No results");
   } else {
@@ -89,27 +96,26 @@ function displayResults(json) {
       let article = document.createElement("article");
       let heading = document.createElement("h2");
       let link = document.createElement("a");
-      let img=document.createElement("img");
-      let para = document.createElement('p'); 
-      let clearfix = document.createElement('div'); //2
+      let img = document.createElement("img");
+      let para = document.createElement("p");
+      let clearfix = document.createElement("div"); //2
 
       let current = articles[index];
-      //console.log("Current", current);
 
       link.href = current.web_url;
       link.textContent = current.headline.main;
 
-      para.textContent="Keywords: ";
-      for(let j = 0; j < current.keywords.length; j++) {
-        let span = document.createElement('span');   
-        span.textContent += current.keywords[j].value + ' ';   
+      para.textContent = "Keywords: ";
+      for (let j = 0; j < current.keywords.length; j++) {
+        let span = document.createElement("span");
+        span.textContent += current.keywords[j].value + " ";
         para.appendChild(span);
       }
-      if(current.multimedia.length > 0) {
-        img.src = 'http://www.nytimes.com/' + current.multimedia[0].url;
+      if (current.multimedia.length > 0) {
+        img.src = "http://www.nytimes.com/" + current.multimedia[0].url;
         img.alt = current.headline.main;
       }
-      clearfix.setAttribute('class','clearfix');
+      clearfix.setAttribute("class", "clearfix");
 
       article.appendChild(heading);
       heading.appendChild(link);
@@ -118,22 +124,20 @@ function displayResults(json) {
       article.appendChild(clearfix);
       section.appendChild(article);
     }
-    //Display the data
   }
 }
 
 function nextPage(e) {
-    pageNumber++; //1
-    fetchResults(e);  //2
-    console.log("Page number:", pageNumber); //3
- }; //5
+  pageNumber++; //1
+  fetchResults(e); //2
+  
+}
 
 function previousPage(e) {
-    if(pageNumber>0){
-        pageNumber--;
-    } else{
-        return;
-    }
-    fetchResults(e);
-    console.log("Previous button clicked");
-} //5
+  if (pageNumber > 0) {
+    pageNumber--;
+  } else {
+    return;
+  }
+  fetchResults(e);
+}
